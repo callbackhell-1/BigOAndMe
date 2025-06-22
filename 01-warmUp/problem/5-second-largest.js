33781eda67ec7c8b7fee000abe0df7d1134311cc
Adarsh <jha01adarsh@gmail.com>
2025-06-23 02:06:17 +0530

    covered corner case

--- 01-warmUp/problem/5-second-largest.js
+++ 01-warmUp/problem/5-second-largest.js
@@ -5,16 +5,20 @@ const secondLargest = (arr) => {
   let secondLargest = Number.NEGATIVE_INFINITY;
 
   for (let i = 0; i < arr.length; i++) {
+if(arr.length<2){
+  return null;
+}
+
     if (arr[i] > largest) {
       secondLargest = largest;
       largest = arr[i];
-    } else if (arr[i] > secondLargest) {
+    } else if (arr[i] > secondLargest && arr[i] !== largest) {
       secondLargest = arr[i];
     }
   }
   return secondLargest;
 };
 
-const arr = [1, -8, 2, -78, 3, 4, -5, 6, 7, -9, 10, 11, -12, 17, 18, 19, -20];
+const arr = [1, -8, 2, -78, 3, 4, -5, 6, 7, -9, 10, 11, -12, 17, 19, 19, -20];
 const result = secondLargest(arr);
 console.log(result);
